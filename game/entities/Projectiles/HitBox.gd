338e946b74aabf5853938d335281c1dcc88b095c
MettBrotDev <florentin@familiedoll.net>
2023-11-23 15:10:47 +0100

    set up structure of ranged attack

---
+++ game/entities/Projectiles/HitBox.gd
@@ -0,0 +1,14 @@
+#HitBox
+class_name HitBox, "HitBox.svg"
+extends Area2D
+
+export var damage := 1
+
+onready var collision_shape := $CollisionShape2D
+
+func _init():
+	collision_mask = 0
+	collision_layer = 2
+	
+func set_disabled(is_disabled: bool) -> void:
+	collision_shape.set_deferred("disabled", is_disabled)
