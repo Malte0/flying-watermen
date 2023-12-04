e482fed5f9562bb3cce9158b25a90bf651541d45
Emre Sözbilir <emresoez@gmail.com>
2023-12-04 23:24:34 +0100

    fixed scripts

--- game/entities/enemies/melee_enemy.gd
+++ game/entities/enemies/melee_enemy.gd
@@ -19,7 +19,7 @@ const DAMAGE_INTERVAL_SECONDS = 0.1
 var damage_timer = 0
 
 # reverse dependency?
-@onready var playerBody: CharacterBody2D = get_tree().get_root().get_node("Main").get_node("Player")
+@onready var playerBody: CharacterBody2D = get_tree().get_first_node_in_group("player")
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
