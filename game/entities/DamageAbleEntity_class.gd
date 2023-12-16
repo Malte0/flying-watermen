5b42be61c16b3a27a2cfdc7a5704e234d69a339c
Malte Wagenknecht <etlamwag@gmail.com>
2023-12-16 01:05:58 +0100

    Refactor DamageAbleEntity and Enemy classes

--- game/entities/DamageAbleEntity_class.gd
+++ game/entities/DamageAbleEntity_class.gd
@@ -2,22 +2,30 @@ class_name DamageAbleEntity extends CharacterBody2D
 
 enum Element { Water, Fire, Neutral }
 
-var entity_health: int = 100
+var max_health: int = 100
+var health: int = 100
 var entity_element: Element = Element.Neutral
 
-func _init(health: int, element: Element):
-	entity_health = health
-	entity_element = element
+signal entity_death
+signal change_health(new_health)
 
-func takeDamage(amount: int, damage_type: Element):
-	if damage_type == entity_element:
+func _init(_entity_element: Element, _max_health: int):
+	max_health = _max_health
+	health = max_health
+	entity_element = _entity_element
+
+func take_damage(amount: int, damage_type: Element):
+	if damage_type != Element.Neutral and damage_type == entity_element:
 		return
-	entity_health -= amount
-	if entity_health <= 0:
+	health -= amount
+	if health <= 0:
 		die()
+	change_health.emit(health)
 
-func restoreHealth(amount: int):
-	entity_health += amount
+func restore_health(health_amount: int):
+	health = maxi(health + health_amount, max_health)
+	change_health.emit(health)
 
 func die():
+	entity_death.emit()
 	queue_free()
