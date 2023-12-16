09319181cda86e3fb178b257175da27b73d56440
Malte Wagenknecht <etlamwag@gmail.com>
2023-12-16 18:12:03 +0100

    add first suite of style-checks

---
+++ .github/workflows/python/main.py
@@ -0,0 +1,31 @@
+import os
+from pathlib import Path
+from style_checks import checkCodeStyle
+
+# functions in dictionary:
+# arguments:
+# lines of the file as list[str] 
+# path to the file as str
+# returns:
+# number of issues found as int
+checksToRun = { "codeStyle": checkCodeStyle }
+
+godotLocation = "../../../game"
+ignoreFolders = [".godot", "addons"]
+
+def run_checks():
+  numberOfIssues = 0
+  for checkName in checksToRun:
+    print("Running check: " + checkName)
+    pathlist = Path(godotLocation).rglob('*.gd')
+    for path in pathlist:
+      if any([ignoreFolder in str(path) for ignoreFolder in ignoreFolders]):
+        continue
+      with open(path, "r") as rf:
+        fileConents = rf.readlines()
+        numberOfIssues += checksToRun[checkName](fileConents, str(path))
+  
+  print("Total issues found: " + str(numberOfIssues))
+
+if __name__== "__main__":
+  run_checks()
---
+++ .github/workflows/python/style_checks.py
@@ -0,0 +1,41 @@
+
+import re
+
+def hasCamelCase(line: str):
+    keyWordsToSearch = ['func', 'var', 'const']
+    words = line.split(" ")
+    for i in range(len(words)):
+        if words[i] in keyWordsToSearch:
+            if re.search(r"[a-z][A-Z]", words[i+1]) is not None:
+                return True
+    return False
+
+maxIndent = 3
+def hasDeepIndentaion(line: str):
+    return line.startswith("\t" * maxIndent)
+
+def usesAutoType(line: str):
+    hasAutoType = re.search(r":=", line) is not None
+    hasNoType = any([keyWord in line for keyWord in ["var", "const"]]) and re.search(r":", line) is None
+    return hasAutoType or hasNoType
+
+checks = {
+    "camelCase": hasCamelCase,
+    "deepIndent": hasDeepIndentaion,
+    "no explicit type": usesAutoType
+}
+
+def checkCodeStyle(lines: list[str], filePath: str):
+    numberOfIssues = 0
+    for lineNumber in range(len(lines)):
+        line = lines[lineNumber]
+        for checkName in checks:
+            if checks[checkName](line):
+                print("Found " + checkName + ":")
+                print("\tFilepath: " + filePath)
+                print("\tLine: " + str(lineNumber))
+                print("\t" + line.strip())
+                print("")
+                numberOfIssues += 1
+
+    return numberOfIssues
