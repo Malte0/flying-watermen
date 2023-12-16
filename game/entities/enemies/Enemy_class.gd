0c01750c38d88e19874fe9ef85dfa522f8e47aff
Malte Wagenknecht <etlamwag@gmail.com>
2023-12-16 01:05:58 +0100

    Refactor DamageAbleEntity and Enemy classes

--- game/entities/enemies/Enemy_class.gd
+++ game/entities/enemies/Enemy_class.gd
@@ -1,25 +1,24 @@
 class_name Enemy extends DamageAbleEntity
 # Use this to define shared behaviour of enemies
 
-enum Movement_Direction { Left = -1, Right = 1 }
+enum Movement_Direction { Left = -1, Right = 1, No = 0 }
+var movement_direction: Movement_Direction = Movement_Direction.No
 
-var movement_speed = 100
+var movement_speed: int = 100
 
 var weight: float = 1
 var gravity: float = ProjectSettings.get_setting("physics/2d/default_gravity")
 var jump_force: int = -500
 
-var has_detected_player = false
+var has_detected_player: bool = false
 
-func _init(_health: int, _weight: float, _jump_force: int, _movement_speed: int):
-	weight = _weight
-	jump_force = _jump_force
-	movement_speed = _movement_speed
-	super(_health, Element.Fire)
+func _init(_max_health):
+	super(Element.Fire, _max_health)
 
 func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += gravity * weight * delta
+	velocity.x = movement_speed * movement_direction;
 	move_and_slide()
 
 #region Movement
@@ -28,8 +27,8 @@ func jump():
 	if is_on_floor():
 		velocity.y = jump_force
 
-func move(direction: Movement_Direction, delta):
-	velocity.x = movement_speed * direction * delta;
+func move(direction: Movement_Direction):
+	movement_direction = direction
 
 #endregion
 
