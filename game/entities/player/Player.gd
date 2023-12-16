34624dce47686371f1d6ab1bb3620a8e12327d5d
Malte Wagenknecht <etlamwag@gmail.com>
2023-12-16 01:10:07 +0100

    refactor various scripts

--- game/entities/player/Player.gd
+++ game/entities/player/Player.gd
@@ -1,14 +1,4 @@
-extends CharacterBody2D
-
-class_name Player
-
-## Emitted when this node is clicked with a mouse
-#signal clicked(node:Node2D)
-
-#func _on_input_event(_viewport:Node, event:InputEvent, _shape_idx:int):
-#	# if the left mouse button is up emit the clicked signal
-#	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.is_pressed() == false:
-#			clicked.emit(self)
+class_name Player extends DamageAbleEntity
 
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var state_chart: StateChart = $StateChart
@@ -16,10 +6,8 @@ class_name Player
 @onready var animation_tree: AnimationTree = $AnimationTree
 @onready var animation_state_machine: AnimationNodeStateMachinePlayback = animation_tree.get("parameters/playback")
 
-
 const ProjectileScene := preload("res://entities/Projectiles/projectile.tscn")
 @onready var shoot_position = $ShootPosition
-
 
 # Reset values
 var base_scale_speed: float = 1.5
@@ -36,16 +24,46 @@ var jump_velocity = base_jump_velocity
 var friction = base_friction
 var fall_speed_factor = base_fall_speed_factor
 var can_move: bool = true
-# health and heat
-var MAX_HEALTH: int = 100
-var health: int = MAX_HEALTH
-const MAX_HEAT: int = 100
+
+##############
+@onready var Heat_reduction_delay = $Timers/Heat_reduction_delay
+@onready var Heat_damage_tick = $Timers/Heat_tick
+@onready var Heal_tick = $Timers/Heal_tick
+
+const Max_heat: int = 100
+const Heat_damage_threshold: int = 90
+const Heat_damage_per_second: int = 5
 var heat: int = 0
-# health timer
-const time_between: float = 0.1
-const heal_over_time_amount: int = 1
-var heal_amount_to_do: int = 0
-var heatprev: int = heat
+
+const Heal_over_time_step = 5
+var heal_over_time_left = 0
+
+func _init():
+	var max_health = 100
+	super(Element.Water, max_health)
+
+# Called to increase the players heat
+func increase_heat(amount: int):
+	heat = mini(heat + amount, Max_heat)
+	Heat_reduction_delay.start()
+
+# Signal called by the heat tick timer
+func _on_heat_tick_timeout():
+	if heat >= Heat_damage_threshold:
+		take_damage(Heat_damage_per_second, Element.Fire)
+	if Heat_reduction_delay.is_stopped():
+		heat = maxi(heat - 1, 0)
+
+# Regenerates healthAmount across healthAmount // Heal_over_time_step seconds
+func heal_over_time(healthAmount: int):
+	heal_over_time_left = healthAmount
+
+# Heals the player for healthAmount during duration seconds
+func _on_heal_tick_timeout():
+	if heal_over_time_left > 0:
+		var amountToHeal = mini(heal_over_time_left, Heal_over_time_step)
+		restore_health(amountToHeal)
+		heal_over_time_left = maxi(heal_over_time_left - Heal_over_time_step, 0)
 
 func reset_variables():
 	speed = base_speed
@@ -60,7 +78,6 @@ const SPRITE_FLIP_OFFSET: int = 0
 var direction: float = 0.0
 var slide_threshold: float = base_speed/2
 
-
 func _ready():
 	animation_tree.active = true
 	#Initialize values so Guards don't complain
@@ -68,7 +85,6 @@ func _ready():
 	state_chart.set_expression_property("air_jumps_left", air_jumps_left)
 	state_chart.set_expression_property("over_slide_threshold", abs(velocity.x) > slide_threshold)
 	state_chart.set_expression_property("velocity_x", velocity.x)
-
 
 func _physics_process(delta):
 	# Debug Info
@@ -90,15 +106,15 @@ func _physics_process(delta):
 				air_jumps_left = air_jumps
 			else:
 				state_chart.send_event("falling")
-	
+
 	if Input.is_action_just_pressed("w"): state_chart.send_event("jump")
-	
+
 	state_chart.set_expression_property("crouching", Input.is_action_pressed("s"))
 	state_chart.set_expression_property("air_jumps_left", air_jumps_left)
 	state_chart.set_expression_property("over_slide_threshold", abs(velocity.x) > slide_threshold)
-	
+
 	update_animation()
-	
+
 	# handle left/right movement
 	direction = Input.get_axis("a", "d")
 	if abs(direction) > 0 and can_move:
@@ -112,67 +128,51 @@ func _physics_process(delta):
 	
 	move_and_slide()
 
-
 func _input(event: InputEvent):
 	if event.is_action_pressed("w"):
 		state_chart.send_event("wPressed")
 	if event.is_action_pressed("attack"):
 		state_chart.send_event("attackpressed")
 
-
 func update_animation():
 	animation_tree.set("parameters/Action/blend_position", abs(velocity.x) > 0)
 	# If player walks in different direction than sprite orienation		
 	if (scale.y > 0 and direction < 0 and velocity.x < 0) or (scale.y < 0 and direction > 0 and velocity.x > 0):
 		flip_player()
-	
 
 func flip_player():
 	scale.x *= -1
 
-
 func facing_wall():
 	return wall_check.is_colliding()
-	
-
-
-
 
 func _on_crouching_state_entered():
 	speed = base_speed/2
-
 
 func _on_sliding_state_entered():
 	friction = base_friction/50
 	can_move = false
 
-
 func _on_airborn_not_coyote_state_input(_event):
 	if Input.is_action_just_pressed("w") and air_jumps_left > 0: air_jumps_left -= 1
 
-
 func _on_jumping_state_entered():
 	velocity.y = jump_velocity + velocity.y/2
-
 
 func _on_wall_slide_state_entered():
 	velocity.y = velocity.y/10
 	fall_speed_factor = base_fall_speed_factor/10
 	#if facing_wall(): flip_player()
 
-
 func _on_pressed_state_physics_processing(_delta):
 	if is_on_floor(): state_chart.send_event("jump")
-
 
 func _on_airborne_state_entered():
 	friction = base_friction/10
 
-
 # Reset variables of any child states of the Movement state
 func _on_movement_child_state_exited():
 	reset_variables()
-
 
 func _on_area_2d_body_entered(body):
 	if body.is_in_group("Hit"):
@@ -181,12 +181,9 @@ func _on_area_2d_body_entered(body):
 	else:
 		pass # Replace with function body.
 
-
 @onready var shape = $AtkShape
 
 func _on_meele_attacks_child_state_entered():
-	#todo (wann wie worum rotieren)
-	#shape.rotate()
 	$AtkShape/CollisionShape2D.disabled = false
 
 func _on_meele_attacks_child_state_exited():
@@ -198,48 +195,5 @@ func _on_cant_shoot_state_entered():
 	projectile_instance.direction = global_position.direction_to(get_global_mouse_position())
 	add_child(projectile_instance)
 
-# health and heat systemsa
-func increase_heat(number: int):
-	heat = mini(number + heat, MAX_HEAT)
-
-func decrease_heat(number: int):
-	heat = maxi(heat - number, 0)
-
-func heal(number: int):
-	health = mini(health + number, MAX_HEALTH)
-	
-func damage(number: int):
-	health = maxi(health - number, 0)
-	if self.health == 0 :
-		get_tree().change_scene_to_file.call_deferred("res://menus/game_over/GameOver.tscn")
-
-func damage_with_scaling(number: int):
-	# using float to avoid division by int warning
-	var heatMaxf := MAX_HEAT as float
-	var scaling_Factor := (number * heat / heatMaxf) as int
-	var new_damage: int = number + scaling_Factor
-	damage(new_damage)
-
-func heal_over_time(totalHeal: int):
-	$Heal_over_time_Timer.wait_time = time_between
-	if ( totalHeal <= heal_over_time_amount):
-		heal(totalHeal)
-	else :
-		heal(heal_over_time_amount)
-		heal_amount_to_do = totalHeal - heal_over_time_amount + heal_amount_to_do
-		$Heal_over_time_Timer.start()
-
-
-func _on_heal_over_time_timer_timeout():
-	if (heal_amount_to_do > 0):
-		var healnum: int = mini(heal_amount_to_do, heal_over_time_amount)
-		heal(healnum)
-		heal_amount_to_do = heal_amount_to_do - healnum
-		$Heal_over_time_Timer.start()
-
-
-
-func _on_reduce_heat_timeout():
-	if heat == heatprev:
-		decrease_heat(1)
-	heatprev = heat
+func _on_entity_death():
+	get_tree().change_scene_to_file.call_deferred("res://menus/game_over/GameOver.tscn")
