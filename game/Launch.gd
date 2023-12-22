16cefe1d869ae7a218d411b5569fdbba12246b36
Simon Döhl <simon.doehl@googlemail.com>
2023-12-22 11:26:22 +0100

    some stuff

--- game/Launch.gd
+++ game/Launch.gd
@@ -2,10 +2,11 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	var window_size_percentage = 0.6
 	var screen_size = DisplayServer.screen_get_size()
 	var screen = DisplayServer.window_get_current_screen()
-	DisplayServer.window_set_size(screen_size * 0.75)
-	DisplayServer.window_set_position((screen_size * 0.25) / 2)
+	DisplayServer.window_set_size(screen_size * window_size_percentage)
+	DisplayServer.window_set_position((screen_size * (1-window_size_percentage)) / 2)
 	DisplayServer.window_set_current_screen(screen)
 	get_tree().change_scene_to_file.call_deferred(Globals.main_scene)
 
--- game/entities/components/health/HealthComponent.gd
+++ game/entities/components/health/HealthComponent.gd
@@ -3,7 +3,7 @@ class_name HealthComponent extends Node2D
 @export var element: Element.Type = Element.Type.Neutral
 @export var max_health: int = 100
 @export var health_bar: TextureProgressBar
-const HEALTH_BAR_INTERPOLATION_SPEED: int = 100
+const HEALTH_BAR_INTERPOLATION_SPEED: int = 1
 var current_health: int = 100
 
 signal health_change(new_health, delta_health)
@@ -12,11 +12,11 @@ signal death
 func _ready():
 	current_health = max_health
 
-func _process(delta):
-	if health_bar != null:
-		if current_health != health_bar.value:
-			var sig: int = sign(current_health - health_bar.value)
-			health_bar.value += sig * delta * HEALTH_BAR_INTERPOLATION_SPEED
+func _process(_delta):
+	if health_bar == null:
+		return
+	if current_health != health_bar.value:
+		health_bar.value = move_toward(health_bar.value, current_health, HEALTH_BAR_INTERPOLATION_SPEED)
 
 func take_damage(amount: int, damage_type: Element.Type):
 	if damage_type != Element.Type.Neutral and damage_type == element:
