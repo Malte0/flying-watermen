4159d2d3484354ea42a0824e52a9ab9816493ba9
Malte Wagenknecht <etlamwag@gmail.com>
2023-12-30 13:41:25 +0100

    refactor heat and health components

--- game/scenes/ui/ui.gd
+++ game/scenes/ui/ui.gd
@@ -1,14 +1,16 @@
 extends CanvasLayer
 
 @onready var debugger: Control = $StateChartDebugger
+@onready var player: Player = get_tree().get_first_node_in_group("player")
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	var player: Player = get_tree().get_first_node_in_group("player")
 	debugger.debug_node(player)
-	$Panel/HealthBar.player = player
 	debugger.visible = false
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("f3"):
 		debugger.visible = !debugger.visible
+
+func _physics_process(_delta):
+	player.state_chart.set_expression_property("velocity_x", player.velocity.x)
+	player.state_chart.set_expression_property("is_on_wall", player.is_on_wall())
