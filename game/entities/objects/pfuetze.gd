997a825f6e9b82d8af0e2d9b969f40fc78d539a8
Malte Wagenknecht <etlamwag@gmail.com>
2024-01-01 18:07:56 +0100

    resolves #22

--- game/entities/objects/pfuetze.gd
+++ game/entities/objects/pfuetze.gd
@@ -1,8 +1,10 @@
 extends Area2D
 
-const AMOUNT_TO_HEAL: int = 25
+var health_in_puddle: int = 25
 
 func _on_body_entered(body):
-	if body is Player: 
-		body.heal_over_time(AMOUNT_TO_HEAL) 
+	if body is Player:
+		var amount_to_heal = body.health_component.max_health - body.health_component.health
+		body.heal_over_time(amount_to_heal)
+		health_in_puddle -= amount_to_heal
 		queue_free()
