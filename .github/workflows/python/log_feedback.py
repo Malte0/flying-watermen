6e1ab9b58850ba302ea3d05b4773c4362a82f562
Malte Wagenknecht <etlamwag@gmail.com>
2024-01-01 17:51:46 +0100

    change log format

--- .github/workflows/python/log_feedback.py
+++ .github/workflows/python/log_feedback.py
@@ -1,7 +1,7 @@
 
 def log(checkName: str, filePath: str, lineNumber: int, line: str):
-    print("Found " + checkName + ":")
-    print("      Filepath: " + filePath.replace("game/", ""))
-    print("      Line: " + str(lineNumber+1))
-    print("      " + line.strip())
+    print(checkName[0].upper() + checkName[1:] + ":")
+    print("    Filepath: " + filePath.replace("game/", ""))
+    print("    Line: " + str(lineNumber+1))
+    print("    " + line.strip())
     print("")
