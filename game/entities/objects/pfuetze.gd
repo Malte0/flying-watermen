f8b04d1f71ed9739b97c717d8f524f6c82ed6c03
Emre Sözbilir <emresoez@gmail.com>
2024-01-09 18:28:43 +0100

    changed health to current_health

--- game/entities/objects/pfuetze.gd
+++ game/entities/objects/pfuetze.gd
@@ -4,7 +4,7 @@ var health_in_puddle: int = 25
 
 func _on_body_entered(body):
 	if body is Player:
-		var amount_to_heal: int = body.health_component.max_health - body.health_component.health
+		var amount_to_heal: int = body.health_component.max_health - body.health_component.current_health
 		body.heal_over_time(amount_to_heal)
 		health_in_puddle -= amount_to_heal
 		queue_free()
