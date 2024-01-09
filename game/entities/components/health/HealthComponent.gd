628cf940595fd0bdbd9a975fe621a05616191029
Marcel John <marcel.john.s@t-online.de>
2024-01-09 15:37:59 +0100

    added iframes

--- game/entities/components/health/HealthComponent.gd
+++ game/entities/components/health/HealthComponent.gd
@@ -5,6 +5,7 @@ class_name HealthComponent extends Node2D
 @export var health_bar: TextureProgressBar
 const HEALTH_BAR_INTERPOLATION_SPEED: int = 1
 var current_health: int = 100
+var can_take_damage: bool = true
 
 signal health_change(new_health: int, delta_health: int)
 signal death()
@@ -21,7 +22,9 @@ func _process(_delta):
 func take_damage(amount: int, damage_type: Element.Type):
 	if damage_type != Element.Type.Neutral and damage_type == element:
 		return
-	current_health -= amount
+	if can_take_damage or not(get_parent() is Player):
+		iframes()
+		current_health -= amount
 	if current_health <= 0:
 		die()
 	health_change.emit(current_health, -amount)
@@ -33,3 +36,9 @@ func restore_health(amount: int):
 func die():
 	death.emit()
 	get_parent().queue_free()
+
+func iframes():
+	can_take_damage = false
+	var iframe_length : float = 1
+	await get_tree().create_timer(iframe_length).timeout
+	can_take_damage = true
