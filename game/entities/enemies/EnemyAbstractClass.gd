b72051c323a2e80fa200367c6e2352a16fc28cdd
Malte Wagenknecht <etlamwag@gmail.com>
2024-01-12 16:41:17 +0100

    change enemy base class

---
+++ game/entities/enemies/EnemyAbstractClass.gd
@@ -0,0 +1,5 @@
+class_name Enemy extends CharacterBody2D
+
+# This class acts as an interface for interactions with enemies
+
+@export var health_component: HealthComponent
--- game/entities/enemies/rangedEnemy/RangedEnemy.gd
+++ game/entities/enemies/rangedEnemy/RangedEnemy.gd
@@ -1,74 +1,40 @@
 extends Enemy
 
-@onready var nodes_to_flip: Node2D = $DirectionalNodes
-@onready var aggro_cooldown_timer: Timer = $AggroCooldown
+@export var aggro_component: AggroComponent
+@export var movement_component: MovementComponent
+@export var movement_speed_calm: int = 100
+@export var movement_speed_aggro: int = 250
+
 @onready var player: Player = get_tree().get_first_node_in_group("player")
 @onready var ground_distance: RayCast2D = $GroundDistance
-const PROJECTILE_SCENE: PackedScene = preload("res://entities/projectiles/FireProjectile.tscn")
-const SHOOTING_PRECISION: float = PI/10 # Angle in radians that gets randomly applied to shots
 
-const MOVEMENT_SPEED_CALM: int = 100
-const MOVEMENT_SPEED_AGGRO: int = 250
-var is_aggro: bool = false
+## When the enemy is directly above the player it stops movement
+# idk what to name this...
+const MOVEMENT_EPSILON_PIXELS: int = 50
 
 func _ready():
-	movement_speed = MOVEMENT_SPEED_CALM
-	move(Movement_Direction.Right)
+	aggro_component.aggro_entered.connect(on_aggro_entered)
+	aggro_component.calm_entered.connect(on_calm_entered)
 
-func _physics_process(delta):
-	if is_aggro:
-		hunt_player()
+func _physics_process(delta: float):
 	if ground_distance.is_colliding():
-		velocity.y = jump_force
-	super(delta)
+		movement_component.jump(1)
 
-func hunt_player():
+	if not aggro_component.is_aggro:
+		return
+
 	var player_distance: float = player.global_position.x - global_position.x
 	var player_direction: int = sign(player_distance)
-	if abs(player_distance) < 20: # this is just some random small value
-		movement_direction = Movement_Direction.No
+	if abs(player_distance) < MOVEMENT_EPSILON_PIXELS:
+		movement_component.movement_direction = movement_component.Movement_Direction.No
 		return
-	if player_direction != movement_direction:
-		flip_move_direction()
-
-func flip_move_direction():
-	if movement_direction == Movement_Direction.Left:
-		move(Movement_Direction.Right)
+	if player_direction == movement_component.Movement_Direction.Right:
+		movement_component.change_move_direction(movement_component.Movement_Direction.Right)
 	else:
-		move(Movement_Direction.Left)
-	nodes_to_flip.scale.x = movement_direction
+		movement_component.change_move_direction(movement_component.Movement_Direction.Left)
 
-func _on_view_area_body_entered(body):
-	if body is Player:
-		become_aggro()
+func on_aggro_entered():
+	movement_component.movement_speed = movement_speed_aggro
 
-func _on_view_area_body_exited(body):
-	if body is Player:
-		aggro_cooldown_timer.start()
-
-func _on_aggro_cooldown_timeout():
-	is_aggro = false
-	movement_speed = MOVEMENT_SPEED_CALM
-
-func become_aggro():
-	aggro_cooldown_timer.stop()
-	is_aggro = true
-	movement_speed = MOVEMENT_SPEED_AGGRO
-
-func _on_health_component_health_changed(_new_health, delta_health):
-	if delta_health < 0:
-		become_aggro()
-
-func _on_direction_change_timeout() -> void:
-	if not is_aggro:
-		flip_move_direction()
-
-func _on_fire_rate_timeout() -> void:
-	if is_aggro:
-		var projectile_instance: Projectile = PROJECTILE_SCENE.instantiate()
-		var random_angle: float = randf()*SHOOTING_PRECISION - (SHOOTING_PRECISION/2)
-		projectile_instance.position = position
-		var new_direction = position.direction_to(player.position).rotated(random_angle)
-		projectile_instance.direction = new_direction
-		projectile_instance.player_speed = velocity
-		get_parent().add_child(projectile_instance)
+func on_calm_entered():
+	movement_component.movement_speed = movement_speed_calm
