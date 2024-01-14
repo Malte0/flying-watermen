598ea6b8e6336533a4837bbf913ab7c8907e8f3d
Emre Sözbilir <emresoez@gmail.com>
2024-01-15 00:25:45 +0100

    map changes + shader

--- game/assets/forest-assets/scenes/stone01.gd
+++ game/assets/forest-assets/scenes/stone01.gd
@@ -2,10 +2,7 @@ extends Sprite2D
 
 var rng = RandomNumberGenerator.new()
 func _ready():
-	var my_random_number = rng.randf_range(0.0, 10.0)
-	# rng.randomize()
-	material.set("shader_parameter/speed", 10 * my_random_number)
-	print(material.get("shader_parameter/speed"))
-	print(material.get("shader_parameter/strengthScale"))
-	print(my_random_number)
-	
+	material.set("shader_parameter/minStrength", rng.randf_range(0.1, 0.2))
+	material.set("shader_parameter/maxStrength", rng.randf_range(0.3,0.5))
+	material.set("shader_parameter/speed", rng.randf_range(1.2,1.6))
+	material.set("shader_parameter/heightOffset", 0.7)
