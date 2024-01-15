c8fc4b0d64ced56e46adfe16e319de5ad8836919
Emre Sözbilir <emresoez@gmail.com>
2024-01-15 21:16:59 +0100

    changed random function

--- game/assets/forest-assets/scenes/greenTree.gd
+++ game/assets/forest-assets/scenes/greenTree.gd
@@ -1,8 +1,8 @@
 extends Sprite2D
 
-var rng = RandomNumberGenerator.new()
+
 func _ready():
-	material.set("shader_parameter/minStrength", rng.randf_range(0.1, 0.4))
-	material.set("shader_parameter/maxStrength", rng.randf_range(0.5,0.7))
-	material.set("shader_parameter/speed", rng.randf_range(0.8,1.4))
+	material.set("shader_parameter/minStrength", randf_range(0.1, 0.4))
+	material.set("shader_parameter/maxStrength", randf_range(0.5,0.7))
+	material.set("shader_parameter/speed", randf_range(0.8,1.4))
 	material.set("shader_parameter/heightOffset", 0.6)
--- game/assets/forest-assets/scenes/stone01.gd
+++
@@ -1,8 +0,0 @@
-extends Sprite2D
-
-var rng = RandomNumberGenerator.new()
-func _ready():
-	material.set("shader_parameter/minStrength", rng.randf_range(0.1, 0.2))
-	material.set("shader_parameter/maxStrength", rng.randf_range(0.3,0.5))
-	material.set("shader_parameter/speed", rng.randf_range(1.2,1.6))
-	material.set("shader_parameter/heightOffset", 0.7)
