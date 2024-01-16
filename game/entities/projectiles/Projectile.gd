067591598d73e6134a622eb7a23f348180d0e5aa
MettBrotDev <florentin@familiedoll.net>
2024-01-16 22:00:42 +0100

    style changes

--- game/entities/projectiles/Projectile.gd
+++ game/entities/projectiles/Projectile.gd
@@ -4,8 +4,8 @@ class_name Projectile extends RigidBody2D
 @export var speed: int = 1500
 @export var damage: int = 20
 @export var sprite: Texture2D
-@export var collisionShape: CollisionShape2D
-@export var collisionMask: int = 5
+@export var collision_shape: CollisionShape2D
+@export var new_collision_mask: int = 5
 
 const LIFE_TIME_SECONDS: int = 4
 var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -13,13 +13,12 @@ var player_speed: Vector2 = Vector2.ZERO
 var direction: Vector2 = Vector2.ZERO
 
 func _ready():
-	get_node("Area2D").collision_mask = collisionMask
-	if collisionShape: $Area2D/CollisionShape2D.set_shape(collisionShape)
+	get_node("Area2D").collision_mask = new_collision_mask
+	if collision_shape: $Area2D/CollisionShape2D.set_shape(collision_shape)
 	if sprite: $Sprite2D.texture = sprite
 	set_linear_velocity((direction * speed) + player_speed)
 	await get_tree().create_timer(LIFE_TIME_SECONDS).timeout
 	queue_free()
-
 
 func _on_area_2d_body_entered(body):
 	var health_component: HealthComponent = body.get_node_or_null("HealthComponent")
@@ -28,4 +27,3 @@ func _on_area_2d_body_entered(body):
 	if body is Player and element != Element.Type.Water:
 		body.heat_component.increase_heat(damage)
 	get_parent().queue_free()
-
