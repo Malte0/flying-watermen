bbf7ab8a0145e16cc3e6f538ff98906fd4d5f25d
Marcel John <marcel.john.s@t-online.de>
2024-01-19 20:47:05 +0100

    fixed style checks

--- game/scenes/ui/main_menu.gd
+++ game/scenes/ui/main_menu.gd
@@ -1,10 +1,9 @@
 class_name MainMenu
 extends Control
 
-
-@onready var start_button = $MarginContainer/HBoxContainer/VBoxContainer/Start_Button as Button
-@onready var exit_button = $MarginContainer/HBoxContainer/VBoxContainer/Exit_Button as Button
-@onready var main_level = preload("res://scenes/test/map.tscn") as PackedScene
+@onready var start_button : Button = $MarginContainer/HBoxContainer/VBoxContainer/Start_Button as Button
+@onready var exit_button : Button = $MarginContainer/HBoxContainer/VBoxContainer/Exit_Button as Button
+@onready var main_level : PackedScene = preload("res://scenes/test/map.tscn") as PackedScene
 
 func _ready():
 	start_button.button_down.connect(on_start_pressed)
