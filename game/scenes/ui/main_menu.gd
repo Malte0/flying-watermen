106c7215e1a7022619588681b880d5090e18547f
Marcel John <marcel.john.s@t-online.de>
2024-01-19 20:13:27 +0100

    added a main menu

---
+++ game/scenes/ui/main_menu.gd
@@ -0,0 +1,17 @@
+class_name MainMenu
+extends Control
+
+
+@onready var start_button = $MarginContainer/HBoxContainer/VBoxContainer/Start_Button as Button
+@onready var exit_button = $MarginContainer/HBoxContainer/VBoxContainer/Exit_Button as Button
+@onready var main_level = preload("res://scenes/test/map.tscn") as PackedScene
+
+func _ready():
+	start_button.button_down.connect(on_start_pressed)
+	exit_button.button_down.connect(on_exit_pressed)
+
+func on_start_pressed() -> void:
+	get_tree().change_scene_to_packed(main_level)
+
+func on_exit_pressed() -> void:
+	get_tree().quit()
