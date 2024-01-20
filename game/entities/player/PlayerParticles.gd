9fc84e2effe6a7b28f0eca09fee16c6ce02b39b6
Emre Sözbilir <emresoez@gmail.com>
2024-01-20 15:06:28 +0100

    visualized damage

--- game/entities/player/PlayerParticles.gd
+++ game/entities/player/PlayerParticles.gd
@@ -3,11 +3,9 @@ extends Node2D
 @onready var player: Player = $".."
 @onready var walk_particles: GPUParticles2D = $WalkingParticles
 @onready var dash_particles: GPUParticles2D = $DashTrail
-@onready var tween = get_tree().create_tween()
 
 func walking_particles():
 	var amount = walk_particles.amount
-	
 	# y > 80 wegen den Wallslides 
 	if abs(player.velocity.x) > 20 || abs(player.velocity.y) > 80 :
 		walk_particles.set_amount_ratio(1)
