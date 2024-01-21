5e6bb69360a3bb22ac6443bcca2f808e9d336dda
Hoai An Nguyen <16hoaian.nguyen@gmail.com>
2024-01-21 20:45:12 +0100

    added stone tablet functionality

--- game/scenes/test/test.gd
+++ game/scenes/test/test.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+@onready var read_stone_tablet_ui = $ReadStoneTablet
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
