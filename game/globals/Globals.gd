1afcf966b2c8e311d64bf3d1a0447196a1a22da8
Simon Döhl <simon.doehl@googlemail.com>
2024-01-21 14:12:15 +0100

    menus folder location

--- game/globals/Globals.gd
+++ game/globals/Globals.gd
@@ -3,7 +3,7 @@ extends Node
 var main_scene: NodePath = "res://scenes/test/map.tscn"
 var test_scene: NodePath = "res://scenes/test/test.tscn"
 var game_over: NodePath = "res://scenes/ui/menus/gameOver/GameOver.tscn"
-var main_menu: NodePath = "res://scenes/ui/main_menu.tscn"
+var main_menu: NodePath = "res://scenes/ui/menus/mainMenu/mainMenu.tscn"
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("str+1"):
--- game/scenes/ui/menus/title/Title.gd
+++
@@ -1,5 +0,0 @@
-extends Control
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	get_tree().change_scene_to_file.call_deferred(Globals.main_menu)
