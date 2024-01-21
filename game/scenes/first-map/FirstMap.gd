e0d8498173041d927506e6761751dd357669a20d
Emre Sözbilir <emresoez@gmail.com>
2024-01-21 21:59:42 +0100

    stone table fix

---
+++ game/scenes/first-map/FirstMap.gd
@@ -0,0 +1,11 @@
+extends Node2D
+@onready var read_stone_tablet_ui = $ReadStoneTablet
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
