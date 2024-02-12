e112f869f0d5f5dfe19cd287eeef14586105d9bb
Simon Döhl <simon.doehl@googlemail.com>
2024-02-12 12:07:15 +0100

    style and loading animation

--- game/scenes/loading/LoadingScreen.gd
+++ game/scenes/loading/LoadingScreen.gd
@@ -2,8 +2,9 @@ class_name LoadingScreen extends Control
 
 @onready var progress_bar: ProgressBar = $ProgressBar
 @onready var texture_progress_bar: TextureProgressBar = $TextureProgressBar
+@onready var animation: AnimatedSprite2D = $AnimatedSprite2D
 
-var progress = []
+var progress: Array = []
 var scene_to_load: String = ""
 var load_status: float = 0
 var highest_progress: int = 0
@@ -11,6 +12,7 @@ var highest_progress: int = 0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	ResourceLoader.load_threaded_request(scene_to_load)
+	animation.play("default")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
