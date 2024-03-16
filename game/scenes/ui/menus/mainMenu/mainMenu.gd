6a026df341db2cdd8eb1173b30dde7891af2efe4
Emre Sözbilir <emresoez@gmail.com>
2024-03-16 15:25:08 +0100

    added sound menu

--- game/scenes/ui/menus/mainMenu/mainMenu.gd
+++ game/scenes/ui/menus/mainMenu/mainMenu.gd
@@ -1,14 +1,21 @@
 class_name MainMenu extends Control
 
 @onready var start_button: Button = $MarginContainer/HBoxContainer/VBoxContainer/Start_Button
+@onready var option_button: Button = $MarginContainer/HBoxContainer/VBoxContainer/Option_Button
 @onready var exit_button: Button = $MarginContainer/HBoxContainer/VBoxContainer/Exit_Button
 
 func _ready():
 	start_button.button_down.connect(on_start_pressed)
+	option_button.button_down.connect(on_option_pressed)
 	exit_button.button_down.connect(on_exit_pressed)
 
 func on_start_pressed() -> void:
 	Globals.load_with_loading_screen(Globals.main_scene)
 
+func on_option_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/ui/menus/optionMenu/optionMenu.tscn")
+
 func on_exit_pressed() -> void:
 	get_tree().quit()
+
+
---
+++ game/scenes/ui/menus/optionMenu/optionMenu.gd
@@ -0,0 +1,24 @@
+extends Control
+
+@onready var back_button = $MarginContainer/HBoxContainer/VBoxContainer/Back_Button
+@onready var music_bus_id = AudioServer.get_bus_index("Music")
+@onready var sfx_bus_id = AudioServer.get_bus_index("SFX")
+@onready var music_slider = $MarginContainer/HBoxContainer/VBoxContainer/GridContainer/MusicSlider
+@onready var sfx_slider = $MarginContainer/HBoxContainer/VBoxContainer/GridContainer/SFXSlider
+
+
+func _ready():
+	music_slider.value = db_to_linear(AudioServer.get_bus_volume_db(music_bus_id))
+	sfx_slider.value = db_to_linear(AudioServer.get_bus_volume_db(sfx_bus_id))
+
+
+func _on_back_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/ui/menus/mainMenu/mainMenu.tscn")
+
+func _on_music_slider_value_changed(value):
+	AudioServer.set_bus_volume_db(music_bus_id, linear_to_db(value))
+	AudioServer.set_bus_mute(music_bus_id, value < 0.05)
+
+func _on_sfx_slider_value_changed(value):
+	AudioServer.set_bus_volume_db(sfx_bus_id, linear_to_db(value))
+	AudioServer.set_bus_mute(sfx_bus_id, value < 0.05)
