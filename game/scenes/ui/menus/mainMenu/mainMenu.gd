e5af52ff28866046339f0f33c0ca48d6b4b4c40d
Simon Döhl <simon.doehl@googlemail.com>
2024-03-19 19:11:38 +0100

    menu stuff and buttons

--- game/scenes/ui/menus/mainMenu/mainMenu.gd
+++ game/scenes/ui/menus/mainMenu/mainMenu.gd
@@ -1,14 +1,7 @@
 class_name MainMenu extends Control
 
-@onready var start_button: Button = $MarginContainer/HBoxContainer/VBoxContainer/Start_Button
-@onready var exit_button: Button = $MarginContainer/HBoxContainer/VBoxContainer/Exit_Button
-
-func _ready():
-	start_button.button_down.connect(on_start_pressed)
-	exit_button.button_down.connect(on_exit_pressed)
-
-func on_start_pressed() -> void:
+func _on_start_pressed() -> void:
 	Globals.load_with_loading_screen(Globals.main_scene)
 
-func on_exit_pressed() -> void:
+func _on_quit_pressed() -> void:
 	get_tree().quit()
