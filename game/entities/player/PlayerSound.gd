ab84f84eac645552a84d8cf0d24215e340d08b01
Emre Sözbilir <emresoez@gmail.com>
2024-03-27 17:26:38 +0100

    sodium sound/ main menu bugs

--- game/entities/player/PlayerSound.gd
+++ game/entities/player/PlayerSound.gd
@@ -4,6 +4,11 @@ extends Node
 
 var item_sound: String = ""
 
+func _on_inventory_on_item_activated(item):
+	if item == null:
+		return
+	item_sound = item.name 
+
 func _on_inventory_on_item_in_inventory_updated(new_item, old_item):
 	if !new_item == null:
 		polyphonic_audio_player.play_sound_effect_from_library("pickup_item")
@@ -15,7 +20,7 @@ func _on_ranged_component_projectile_shoot():
 	if item_sound == "foam":
 		polyphonic_audio_player.play_sound_effect_from_library("foam")
 	if item_sound == "sodium":
-		polyphonic_audio_player.play_sound_effect_from_library("foam")
+		polyphonic_audio_player.play_sound_effect_from_library("jump")
 	else:
 		polyphonic_audio_player.play_sound_effect_from_library("shoot")
 
@@ -25,15 +30,6 @@ func _on_jumping_state_entered():
 func _on_dash_state_entered():
 	polyphonic_audio_player.play_sound_effect_from_library("dash")
 
-func _on_inventory_on_item_activated(item):
-	if item == null:
-		return
-	item_sound = item.name 
-
 func _on_inventory_on_item_used(amount_left, max_amount):
 	if amount_left == 0:
 		item_sound = ""
-
-func sodium_shoot():
-	pass
-	
--- game/scenes/ui/menus/menu.gd
+++ game/scenes/ui/menus/menu.gd
@@ -1,6 +1,6 @@
 class_name Menu extends Control
 
-@onready var main: MainMenu = $Main
+@onready var main: MainMenu = $MainMenu
 @onready var option: OptionsMenu = $Options
 @onready var menu_audio_player: AudioStreamPlayer = $MenuAudioPlayer
 @onready var menu_music = preload("res://assets/SFX/worldMusic/Ambient 5.wav")
