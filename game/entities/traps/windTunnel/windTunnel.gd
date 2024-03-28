2b5af2022352c65958af7d1df0d6928ba8180da4
Simon Döhl <simon.doehl@googlemail.com>
2024-03-28 23:15:30 +0100

    kinda tooly

--- game/entities/traps/windTunnel/windTunnel.gd
+++ game/entities/traps/windTunnel/windTunnel.gd
@@ -1,16 +1,30 @@
+@tool
 extends Area2D
 
 @onready var player = get_tree().get_first_node_in_group("Player")
 
 @export var force: float = 0
 @export var lifetime: float = 0
-@export var collision_shape: CollisionShape2D
+@export var collision_shape: CollisionShape2D:
+	set(value):
+		collision_shape = value
+		fit_particle()
 
 var force_vector: Vector2:
 	get: return Vector2(force, 0).rotated(rotation)
 var bodys_in_wind_tunnel: Array = []
 
+func fit_particle():
+	var partic: GPUParticles2D = $GPUParticles2D
+	partic.lifetime = lifetime
+	partic.process_material.emission_box_extents = Vector3(0, collision_shape.shape.size.y, 0)
+	partic.set_visibility_rect(Rect2(Vector2(0,0), collision_shape.shape.size))
+	partic.amount = collision_shape.position.x / 3
+	global_position = collision_shape.global_position - Vector2(collision_shape.shape.get_rect().size.x/2, 0).rotated(rotation)
+	collision_shape.position = Vector2(collision_shape.shape.get_rect().size.x/2,0)
+
 func _physics_process(delta):
+	if Engine.is_editor_hint(): return
 	for body in bodys_in_wind_tunnel:
 		if body is CharacterBody2D:
 			_apply_wind_to_char_body(body, delta)
@@ -34,8 +48,4 @@ func _on_body_exited(body):
 		bodys_in_wind_tunnel.erase(body)
 
 func _ready():
-	var partic: GPUParticles2D = $GPUParticles2D
-	partic.lifetime = lifetime
-	partic.process_material.emission_box_extents = Vector3(0, collision_shape.shape.size.y, 0)
-	partic.set_visibility_rect(Rect2(Vector2(0,0), collision_shape.shape.size))
-	partic.amount = collision_shape.position.x / 3
+	fit_particle()
