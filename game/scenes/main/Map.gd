70d57714849bd983b6847996e0f3bb9ba937bb52
Emre Sözbilir <emresoez@gmail.com>
2024-04-01 22:22:26 +0200

    map fix

--- game/scenes/main/Map.gd
+++ game/scenes/main/Map.gd
@@ -6,18 +6,6 @@ func _on_destroyable_wall_3_wall_destroyed():
 	$WindTunnels/WindTunnel3/CollisionShape2D.set_deferred("disabled", false)
 	$WindTunnels/WindTunnel3.visible = true
 
-func _on_blind_begining_body_entered(body):
-	blinging_player(body)
-
-func _on_vision_beginning_body_entered(body):
-	give_vision_to_player(body)
-
-func _on_bilnd_ending_body_entered(body):
-	blinging_player(body)
-
-func _on_vision_ending_body_entered(body):
-	give_vision_to_player(body)
-
 func blinging_player(body):
 	if body is Player:
 		pass
