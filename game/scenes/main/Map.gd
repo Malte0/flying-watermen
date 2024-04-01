336156f380827983b61db9d580009248b8e57e1b
Simon Döhl <simon.doehl@googlemail.com>
2024-04-01 22:35:07 +0200

    fix error

--- game/scenes/main/Map.gd
+++ game/scenes/main/Map.gd
@@ -1,7 +1,5 @@
 extends Node2D
 
-@onready var vision_obstacle: PointLight2D = $Player/PointLight2D2
-
 func _on_destroyable_wall_3_wall_destroyed():
 	$WindTunnels/WindTunnel3/CollisionShape2D.set_deferred("disabled", false)
 	$WindTunnels/WindTunnel3.visible = true
