964b0a56b102c08da031339e24b6f4e5ebbd78ea
Simon Döhl <simon.doehl@googlemail.com>
2024-04-02 12:01:01 +0200

    Fix ice

--- game/entities/player/Player.gd
+++ game/entities/player/Player.gd
@@ -294,6 +294,3 @@ func toggle_light():
 		tween.tween_property(point_light, "energy", 0, 0.5)
 	else:
 		tween.tween_property(point_light, "energy", 0.7, 0.5)
-
-func _on_chemical_effects_manager_ice_entered() -> void:
-	%AnimatedSprite2D.play("ice")
